28fb69225c84b39c42da214fb168f902a23096b2
Levi Gleik <leviacedo1@gmail.com>
2025-07-16 20:00:15 -0300

    chore: add next-themes and tooltip - add next-theme provider - rename react-query provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,18 @@
 import type { PropsWithChildren } from "react";
-import ReactQueryProvider from "./react-query";
+import ReactQueryProvider from "./react-query-provider";
+import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: PropsWithChildren) {
-	return <ReactQueryProvider>{children}</ReactQueryProvider>;
+	return (
+		<ReactQueryProvider>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				{children}
+			</ThemeProvider>
+		</ReactQueryProvider>
+	);
 }
